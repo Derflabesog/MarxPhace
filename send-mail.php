c0b85a0920ca82da2942708d53b05c785cc4d56b
Polly <wildernesafrica@gmail.com>
2026-01-17 09:15:06 +0300

    Update site

--- send-mail.php
+++ send-mail.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = "admin@rimonnamibia.com";
+$to = "marxphase@gmail.com";
 $subject = "Website Contact Form";
 $headers = "From: $email";
 
